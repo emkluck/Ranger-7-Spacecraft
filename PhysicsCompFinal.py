2c3a5d028e20306516c436ef04e572cdbe8500ff
emkluck <107065367+emkluck@users.noreply.github.com>
2023-11-16 09:29:03 -0700

    Update PhysicsCompFinal.py

--- PhysicsCompFinal.py
+++ PhysicsCompFinal.py
@@ -1,6 +1,6 @@
 Web VPython 3.2
 from visual import *
-earth = sphere(pos=vector(0,0,0), radius = 10, color=color.cyan)
-moon = sphere(pos=vector(0,60,0), radius = 4, color=color.white)
-rocket = sphere(pos=vector(0,50,0), radius = 2, color=color.red)
+earth = sphere(pos=vector(0,0,0), radius = 6371, color=color.cyan)
+moon = sphere(pos=vector(0,38440,0), radius = 1737.4, color=color.white)
+rocket = sphere(pos=vector(0,50+6371+500,0), radius = 500, color=color.red)
 
